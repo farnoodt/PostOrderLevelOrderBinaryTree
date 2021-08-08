3d9561bcfec8be6ef427f12482a853f7e5fa4f7c
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-07 21:39:16 -0700

    Add project files.

---
+++ PostOrderLevelOrderBinaryTree/LevelOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOrderLevelOrderBinaryTree
+{
+    class LevelOrder
+    {
+        public Stack<int> PostOrder(Node root)
+        {
+            Stack<Node> S1 = new Stack<Node>();
+            Stack<int> S2 = new Stack<int>();
+
+            S1.Push(root);
+
+            while (S1.Count != 0)
+            {
+                root = S1.Pop();
+                S2.Push(root.data);
+
+                if(root.left != null)
+                    S1.Push(root.left); 
+
+                if (root.right != null)
+                    S1.Push(root.right);
+            }
+
+            return S2;
+        }
+    }
+}
---
+++ PostOrderLevelOrderBinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PostOrderLevelOrderBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(1);
+            tree.left = new Node(-1);
+            tree.right = new Node(11);
+            tree.right.left = new Node(21);
+            tree.right.right = new Node(6);
+            tree.left.left = new Node(-2);
+            tree.left.right = new Node(-3);
+            tree.left.right.right = new Node(5);
+
+            LevelOrder LO = new LevelOrder(); 
+
+            Console.WriteLine(string.Join( " , " , LO.PostOrder(tree)));
+            Console.ReadKey();
+        }
+    }
+}
